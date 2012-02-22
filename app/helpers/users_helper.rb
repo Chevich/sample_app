aa9c154d3ce68f8feded821d2e28e915dc72d12b
Andy.Chevich <Andy.Chevich@gmail.com>
2012-02-22 14:27:39 +0300

    Heroku

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
   def gravatar_tag(user, options = { :size => 50 })
-    gravatar_image_tag user.email, :class => "gravatar", 
-        :alt => user.name.capitalize+' avatar',
+    gravatar_image_tag user.email.downcase, :class => "gravatar",
+        :alt => user.name+' avatar',
         :gravatar => options
-  end 
+  end
 
 end
